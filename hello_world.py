782bd717f5d3bcc9066ef31821fd2a45760d35c3
Gar Ken Ng <108896650+garkenng@users.noreply.github.com>
2026-06-23 09:01:10 -0400

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,8 @@
+On the right, here, there's typically the exercise instructions, for example: "Write a single line of Python code to display Hello world!".
+
+The code to solve the exercise is to be written in the black code editor, on the left of the page.
+
+But as it's just an example, I already wrote the code for you ☺
+
+Below the code editor you can see a Submit button, use it to send your answer to the correction bot. The bot will review and test your code, and will tell you if it's valid of if you have something to enhance.
 print("Hello world!")
